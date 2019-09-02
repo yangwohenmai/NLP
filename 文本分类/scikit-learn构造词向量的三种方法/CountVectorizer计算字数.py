20825074f2b4ed80b051841a26924681be4b461a
yangwohenmai <zongxianyang@outlook.com>
2019-09-02 17:47:17 +0800

    update

--- 文本分类/scikit-learn构造词向量的三种方法/CountVectorizer计算字数.py
+++ 文本分类/scikit-learn构造词向量的三种方法/CountVectorizer计算字数.py
@@ -1,7 +1,18 @@
 from sklearn.feature_extraction.text import CountVectorizer
+"""
+原理：
+1.先对文本词汇进行索引编码，构造 单词-索引 字典
+2.文本索引构建原理为：按照单词字母顺序排序，a排在前，z排在后
+3.将每个文本编辑成一个一维向量
+向量的长度为词汇表中的总单词数
+向量中元素排序为索引顺序从小到大排序
+向量中元素数字是单词出现的次数
+dic：{a:1,b:2,c:3,d:4,e:5,:f:6,g:7}
+text = bcfadde
+vec = [1,1,1,2,1,1,0]  2代表d出现2次，0代表g没出现过，1代表出现过一次的单词
+"""
 # list of text documents
 text = ["The quick brown fox jumped over the lazy dog."]
-#text = ["the puppy"]
 # create the transform
 vectorizer = CountVectorizer()
 # tokenize and build vocab
@@ -9,7 +20,7 @@ vectorizer.fit(text)
 # summarize
 print(vectorizer.vocabulary_)
 # encode document
-vector = vectorizer.transform(text)
+vector = vectorizer.transform([text[0]])
 # summarize encoded vector
 print(vector.shape)
 print(type(vector))
