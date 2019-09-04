721983d808e42663e5af66afcefd26cbb4631cd9
yangwohenmai <zongxianyang@outlook.com>
2019-09-04 17:10:01 +0800

    update

--- 文本分类/用Embidding层生成词向量/Embidding层是如何学习的.py
+++ 文本分类/用Embidding层生成词向量/Embidding层是如何学习的.py
@@ -5,7 +5,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers.embeddings import Embedding
-# define documents
+"""
+本文构建了一个 Embedding层-过渡层-分类层 的简单3层网络，
+通过训练网络，使得Embedding层学习出对应的权重矩阵，作为“嵌入层参数”，可供以后直接使用
+数据构造方法:
+1.定义词汇量
+2.调用one_hot()，根据词汇量对文本进行onehot编码
+3.调用pad_sequences(),将编码后的向量补全成相同长度
+4.构建网络训练Embedding层
+5.训练得到Embedding层的权重矩阵就是嵌入层参数
+"""
+# 10个文本
 docs = ['Well done!',
 		'Good work',
 		'Great effort',
@@ -30,6 +40,7 @@ padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
 print(padded_docs)
 
 model = Sequential()
+# 定义嵌入层，参数：词汇量，每个单词8维度，每个文本长度为4
 model.add(Embedding(vocab_size, 8, input_length=max_length))
 model.add(Flatten())
 model.add(Dense(1, activation='sigmoid'))
